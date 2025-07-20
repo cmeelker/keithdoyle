bc723fe2c201a304dbc5dcb3290c875e02802eaf
Christa Meelker <christameelker@gmail.com>
2025-07-20 12:41:36 +0200

    Use circle as toggle icon

--- app/_lib/components/FontSizeSlider.tsx
+++ app/_lib/components/FontSizeSlider.tsx
@@ -19,7 +19,7 @@ export default function FontSizeSlider() {
         step={1}
         value={value}
         onChange={(e) => onChange(Number(e.target.value))}
-        className="h-[2px] w-full cursor-pointer appearance-none rounded-lg bg-black [&::-webkit-slider-thumb]:h-[10px] [&::-webkit-slider-thumb]:w-[42px] [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-sm [&::-webkit-slider-thumb]:bg-black [&::-webkit-slider-thumb]:shadow"
+        className="h-[2px] w-full cursor-pointer appearance-none rounded-lg bg-black [&::-webkit-slider-thumb]:h-[var(--font-size)] [&::-webkit-slider-thumb]:w-[var(--font-size)] [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-black [&::-webkit-slider-thumb]:shadow"
       />
     </div>
   );
